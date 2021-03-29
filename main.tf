165a229eb61dcde10c77021f15625e2c5dbf26e8
Tushar Shrivastava <tushar.shrivastava@afourtech.com>
2021-03-30 00:52:29 +0530

    adding main config file for aws

--- main.tf
+++ main.tf
@@ -1,1 +1,25 @@
-#Main Terraform file
+#Main Terraform Config file
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+    
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-068d43a544160b7ef"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "New Instance"
+  }
+}
